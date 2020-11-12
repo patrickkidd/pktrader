ac7b1542eddba1a7d81b230251fd22c304c93d68
Patrick Stinson <patrick@vedanamedia.com>
2020-11-11 22:27:50 -0900

    Fixed pyinstaller build

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # pktrader
 Algorithmic Trading App
+
+
+
+```
+brew install readline xz
+CONFIGURE_OPTS=--enable-shared pyenv install 3.7.8
+```
+
--- bin/build.sh
+++ bin/build.sh
@@ -4,14 +4,13 @@ set -e # exit on first error
 
 TARGET=$1
 
-BIN="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-ROOT=`cd "$BIN/.."; pwd`
-
 
 if [[ $TARGET = "osx" ]]; then
 
-	pyinstaller --onedir --windowed --noconfirm main.spec
-	codesign -s "Developer ID Application: Patrick Stinson (8KJB799CU7)" dist/PKTrader.app
+	# pyinstaller --onedir --windowed --noconfirm main.spec
+	rm -rf ./build ./dist
+	pyinstaller --onedir --windowed --noconfirm PKTrader.spec
+	codesign --deep -s "Developer ID Application: Patrick Stinson (8KJB799CU7)" dist/PKTrader.app
 	open ./dist
 
 elif [[ $TARGET == "clean" ]]; then
